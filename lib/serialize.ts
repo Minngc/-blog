5c1a31807beb1004fa16abf02324cb87d38bd3de
Minngc <ming109152@gmail.com>
2023-05-14 21:13:06 +0800

    some new plugin & get toc

--- lib/serialize.ts
+++ lib/serialize.ts
@@ -3,24 +3,72 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings/lib";
 import rehypeToc from "rehype-toc";
 import rehypeSlug from "rehype-slug";
 import { serialize } from "next-mdx-remote/serialize";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+// import Tag from "pages/tag";
 
 const serializeWithPlugin = async (content: string) => {
-  return await serialize(content, {
+  let tocHead: (
+    | { type: "nolist"; href: string; value: string }
+    | {
+        type: "haslist";
+        href: string;
+        value: string;
+        children: { href: string; value: string }[];
+      }
+  )[] = [];
+  const serializeContent = await serialize(content, {
     mdxOptions: {
-      remarkPlugins: [remarkFrontmatter],
+      remarkPlugins: [remarkFrontmatter, remarkGfm, remarkMath],
       rehypePlugins: [
         rehypeSlug,
         [
           rehypeAutolinkHeadings,
           {
-            behavior: "append",
-            content: { type: "text", value: "" },
+            behavior: "wrap",
+            // content: { type: "text", value: "" },
           },
         ],
-        [rehypeToc, { headings: ["h2", "h3", "h4", "h5", "h6"] }],
+        [
+          rehypeToc,
+          {
+            headings: ["h2", "h3"],
+            nav: false,
+            customizeTOC: (toc: any) => {
+              if (toc.children) {
+                const H2List = toc.children;
+                tocHead = H2List.map((value: any) => {
+                  console.log(value.children[0]);
+                  if (value.children.length && value.children.length === 2) {
+                    const H3List = value.children[1].children;
+                    const H3Title = H3List.map((value: any) => {
+                      return {
+                        href: value.children[0].properties.href,
+                        value: value.children[0].children[0].value,
+                      };
+                    });
+                    return {
+                      type: "haslist",
+                      href: value.children[0].properties.href,
+                      value: value.children[0].children[0].value,
+                      children: H3Title,
+                    };
+                  } else
+                    return {
+                      type: "nolist",
+                      href: value.children[0].properties.href,
+                      value: value.children[0].children[0].value,
+                    };
+                });
+              }
+              return false;
+            },
+          },
+        ],
       ],
     },
   });
+  return [serializeContent, tocHead] as const;
 };
 
 export { serializeWithPlugin };

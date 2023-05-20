ced07afb4452fa6071a979e095d2db9dcf673c55
Minngc <ming109152@gmail.com>
2023-05-20 22:12:56 +0800

    article  Card

---
+++ components/card/articleCard/index.tsx
@@ -0,0 +1,59 @@
+import classNames from "classnames";
+import styles from "./index.module.scss";
+import { MatterType } from "lib/getPostFiles";
+import Link from "next/link";
+import { AngleIcon } from "@/Icon";
+
+const ArticleCard = (props: { data: MatterType; links: string }) => {
+  const {
+    data: {
+      Id: id,
+      Author: author,
+      Title: title,
+      Tag: tag,
+      Date: date,
+      Description: description,
+    },
+    links: link,
+  } = props;
+  return (
+    <div className={classNames([styles.cardContainer])}>
+      <div className={classNames([styles.card2])}>
+        <div className={classNames([styles.mainContent])}>
+          <div className={classNames([styles.title])}>
+            <Link href={link} title={title}>{title}</Link>
+          </div>
+          <div className={classNames([styles.author])}>{author}</div>
+          <div className={classNames([styles.containerOfTagADate])}>
+            <div className={classNames([styles.tag])}>
+              {tag.map((value) => {
+                return (
+                  <Link
+                    key={`tag_link_${value}`}
+                    href={"./"}
+                    className={styles.tagContainer}
+                  >
+                    {value}
+                  </Link>
+                );
+              })}
+            </div>
+            <div className={classNames([styles.date])}>{date}</div>
+          </div>
+        </div>
+
+        <div>
+          <div className={classNames([styles.description])}>{description}</div>
+          <div className={classNames([styles.cardFooter])}>
+            <Link href={link}>
+              查看全文
+              <AngleIcon className={styles.cardAngel} height={16} width={16} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleCard;

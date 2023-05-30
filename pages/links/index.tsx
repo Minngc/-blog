fdb2e02ee02149820652dbf2d508316250e98142
Minngc <ming109152@gmail.com>
2023-05-30 23:00:08 +0800

    links

--- pages/links/index.tsx
+++ pages/links/index.tsx
@@ -1,5 +1,44 @@
+import { AngleIcon } from "@/Icon";
+import link from "../../config/json/link.json";
+import Image from "next/image";
+import styles from "@styles/Link.module.scss";
+
 const Link = () => {
-  return <>这个页面会用来展示友链</>;
+  return (
+    <>
+      <ul className={styles.linkList}>
+        {link.map((value, index) => {
+          return (
+            <li key={`${value.link}_${index}`}>
+              <div className={styles.avator}>
+                <Image
+                  className={styles.avator}
+                  src={value.avator}
+                  alt={value.name}
+                  width={60}
+                  height={60}
+                />
+              </div>
+              <div className={styles.content}>
+                <div className={styles.blogName}>{value.name}</div>
+                <div className={styles.description}>{value.description}</div>
+              </div>
+              <div className={styles.link}>
+                <a target="about_blank" href={value.link}>
+                  查看博客
+                  <AngleIcon
+                    className={`${styles.angelRolated}`}
+                    width={16}
+                    height={16}
+                  />
+                </a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default Link;

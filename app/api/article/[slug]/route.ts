03b598a8728179e8515ad0215616d8009b66ec80
Minngc <ming109152@gmail.com>
2023-07-25 16:08:47 +0800

    styles and types

--- app/api/article/[slug]/route.ts
+++ app/api/article/[slug]/route.ts
@@ -12,16 +12,36 @@ export async function GET(
     params: { slug: string };
   }
 ) {
-  console.log(params.slug);
   const path = (article as any)[`${params.slug}`];
   try {
     const fileContent = fs.readFileSync(path, "utf-8");
 
     const matterContent = matter(fileContent);
-    const frontmatter = matterContent.data;
+    const originMatter = matterContent.data;
+    const frontMatter = {
+      author: originMatter.Author,
+      cover: originMatter.Cover,
+      date: originMatter.Date,
+      description: originMatter.Description,
+      link: originMatter.Link,
+      tag: originMatter.Tag,
+      title: originMatter.Title,
+    };
     const [content, tocHead] = await serializeWithPlugin(matterContent.content);
 
-    return NextResponse.json({ frontmatter, content, tocHead });
+    return NextResponse.json({ frontMatter, content, tocHead } as {
+      frontMatter: {
+        author: string;
+        cover?: string;
+        date: string;
+        description: string;
+        link: string;
+        tag: string[];
+        title: string;
+      };
+      content: any;
+      tocHead: any;
+    });
   } catch {
     return NextResponse.json({ error: "文章不存在" });
   }

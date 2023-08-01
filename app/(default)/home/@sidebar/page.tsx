7c4898c57e21945e6d9298cf3b72223ff67dca23
Minngc <ming109152@gmail.com>
2023-08-01 23:14:05 +0800

    home & @sideBar

--- app/(default)/home/@sidebar/page.tsx
+++ app/(default)/home/@sidebar/page.tsx
@@ -1,88 +1,90 @@
-import classNames from "classnames";
 import styles from "./page.module.scss";
-import { Line } from "@/components/line";
-import { Avatar } from "@/components/avatar";
+import { SideBarTag } from "@/components/tag";
+import { AnimeCard } from "@/components/animeCard";
+import { LogCard } from "@/components/logCard";
+import { SideBarCard } from "@/components/sideBar";
+
+const notice =
+  "博客建设工作仍在进行中，已经发布一些测试博文，之后会发布更多博文。";
+
+const tags = [
+  {
+    title: "学习",
+    list: [
+      { tagName: "javascript" },
+      { tagName: "cs" },
+      { tagName: "se" },
+      { tagName: "go" },
+    ],
+  },
+  { title: "记录", list: [{ tagName: "widows" }] },
+  { title: "攻略", list: [{ tagName: "shadowverse" }, { tagName: "元神" }] },
+  { title: "日常", list: [{ tagName: "note" }] },
+];
+
+const anime = [
+  {
+    title: "异世界XX冒险录",
+    state: "月曜日",
+    type: ["轻改", "异世界", "后宫"],
+  },
+  {
+    title: "异世界XX冒险录",
+    state: "月曜日",
+    type: ["轻改", "异世界", "后宫"],
+  },
+  {
+    title: "异世界XX冒险录",
+    state: "月曜日",
+    type: ["轻改", "异世界", "后宫"],
+  },
+];
+
+const log = [
+  {
+    date: "08/01/2023",
+    logMsg: "抽象 SideBar 内容为组件",
+  },
+  {
+    date: "07/29/2023",
+    logMsg: "制作了新的 SideBar",
+  },
+];
 
 const HomeSidebar = () => {
   return (
     <>
-      <div className={classNames(styles.notice)}>
-        <div className={classNames(styles.noticeHeader)}>
-          <div className={styles.noticeTitle}>公告</div>
-          <div className={styles.noticeDate}>07/29/2023</div>
-        </div>
-        <Line />
-        <div className={classNames(styles.noticeContainer)}>
-          博客建设工作仍在进行中，已经发布一些测试博文，之后会发布更多博文。
-        </div>
-      </div>
+      <SideBarCard
+        title="公告"
+        sideNode={<div className={styles.noticeDate}>07/29/2023</div>}
+        className={{ cardContainer: styles.noticeContainer }}
+      >
+        {notice}
+      </SideBarCard>
 
-      <div className={classNames(styles.notice)}>
-        <div className={classNames(styles.noticeHeader)}>
-          <div className={styles.noticeTitle}>アニメ</div>
-          {/* <div>07/29/2023</div> */}
-        </div>
-        <Line />
-        <div className={classNames(styles.animeContainer)}>
-          <div className={classNames(styles.animeCard)}>
-            <Avatar size={70} />
-            <div className={classNames(styles.animeMessage)}>
-              <div className={classNames(styles.animeTitle)}>
-                异世界XX冒险录
-              </div>
-              <div className={classNames(styles.animeType)}>
-                类型: 轻改 异世界 后宫
-              </div>
-              <div className={classNames(styles.animeState)}>
-                连载状态: 月曜日
-              </div>
-            </div>
-          </div>
+      <SideBarCard title="标签">
+        {tags.map(({ title, list }) => {
+          return (
+            <SideBarTag
+              key={`sidebarTags_${title}`}
+              title={title}
+              tagList={list}
+            />
+          );
+        })}
+      </SideBarCard>
 
-          <div className={classNames(styles.animeCard)}>
-            <Avatar size={70} />
-            <div className={classNames(styles.animeMessage)}>
-              <div className={classNames(styles.animeTitle)}>
-                异世界XX冒险录
-              </div>
-              <div className={classNames(styles.animeType)}>
-                类型: 轻改 异世界 后宫
-              </div>
-              <div className={classNames(styles.animeState)}>
-                连载状态: 月曜日
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SideBarCard title="アニメ">
+        {anime.map((value) => {
+          return <AnimeCard key={`${value.title}_${value.state}`} {...value} />;
+        })}
+      </SideBarCard>
 
-      <div className={classNames(styles.notice)}>
-        <div className={classNames(styles.noticeHeader)}>
-          <div className={styles.noticeTitle}>标签</div>
-          {/* <div>07/29/2023</div> */}
-        </div>
-        <Line />
-        <div className={classNames(styles.tagContainer)}>
-          <div className={classNames(styles.tag)}>
-            <div>language:</div>
-            <div>#tag1 #tag2 #tag3</div>
-          </div>
-        </div>
-      </div>
-
-      <div className={classNames(styles.notice)}>
-        <div className={classNames(styles.noticeHeader)}>
-          <div className={styles.noticeTitle}>日志</div>
-          {/* <div>07/29/2023</div> */}
-        </div>
-        <Line />
-        <div className={classNames(styles.logContainer)}>
-          <div className={classNames(styles.log)}>
-            <div className={classNames(styles.logDate)}>2023-07-29:</div>
-            <div className={classNames(styles.logMsg)}>制作了新的 SideBar</div>
-          </div>
-        </div>
-      </div>
+      <SideBarCard title="日志">
+        {log.map((value) => {
+          return <LogCard key={`logCard_${value.date}`} {...value} />;
+        })}
+      </SideBarCard>
     </>
   );
 };

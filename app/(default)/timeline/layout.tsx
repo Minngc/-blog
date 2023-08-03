5fe7cdb3bce9616dad7bd7246ae5d37f25ddcf61
Minngc <ming109152@gmail.com>
2023-08-03 17:24:52 +0800

    timeLine & SideBar

---
+++ app/(default)/timeline/layout.tsx
@@ -0,0 +1,17 @@
+import classNames from "classnames";
+import { ReactNode } from "react";
+import styles from "./page.module.scss";
+
+const Layout = (props: { children: ReactNode; sidebar: ReactNode }) => {
+  const { children, sidebar } = props;
+  return (
+    <>
+      <div className={classNames(styles.timeLine)}>
+        <div className={classNames(styles.timeLineContainer)}>{children}</div>
+        <div className={classNames(styles.sideBar)}>{sidebar}</div>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
--- app/(default)/timeline/page.tsx
+++ app/(default)/timeline/page.tsx
@@ -1,8 +1,59 @@
+import classNames from "classnames";
+import styles from "./page.module.scss";
+import listOrderByYear from "@/config/listOrderByYear.json";
+import { useSearchParams } from "next/navigation";
+import { Line } from "@/components/line";
+import Link from "next/link";
+
 const Timeline = () => {
+  const years = Object.keys(listOrderByYear).sort(
+    (a, b) => Number(b) - Number(a)
+  );
+
   return (
     <>
-      <div>草图画好了，下次就写。</div>
+      {years.map((year) => {
+        return (
+          <TimeLineItem
+            key={`timeline_item_${year}`}
+            year={year}
+            list={(listOrderByYear as any)[year]}
+          />
+        );
+      })}
     </>
+  );
+};
+
+const TimeLineItem = (props: {
+  year: string;
+  list: {
+    year: string;
+    month: string;
+    title: string;
+    link: string;
+    date: string;
+  }[];
+}) => {
+  const { list, year } = props;
+  return (
+    <div className={classNames(styles.yearContainer)}>
+      <div className={classNames(styles.title)}>{year}</div>
+      <Line />
+
+      <ul className={styles.postList}>
+        {list.map(({ year, month, title, link, date }) => {
+          return (
+            <li key={`${year}_${month}_`} className={styles.postItem}>
+              <Link href={`./article/${year}/${month}/${link}`}>
+                <span className={styles.postTitle}>{title}</span>
+                <span className={styles.postDate}>({date})</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 

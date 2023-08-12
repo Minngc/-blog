8c06f3070dd6f412b23209049c05ca77a0b325a1
Minngc <ming109152@gmail.com>
2023-08-12 22:48:47 +0800

    table elements

--- app/(default)/article/[...slug]/page.tsx
+++ app/(default)/article/[...slug]/page.tsx
@@ -7,6 +7,10 @@ import useSWR from "swr";
 
 type ArticlePath = [year: string, month: string, title: string];
 
+const H1 = (props: any) => {
+  return <></>;
+};
+
 const H2 = (props: any) => {
   return (
     <h2 className="article-ele-h2" id={props.id}>
@@ -67,6 +71,30 @@ const Li = (props: any) => {
   return <li className="article-ele-li">{props.children}</li>;
 };
 
+const Table = (props: any) => {
+  return (
+    <div className="article-ele-table--container">
+      <table className="article-ele-table">{props.children}</table>
+    </div>
+  );
+};
+
+const THead = (props: any) => {
+  return <thead className="article-ele-thead">{props.children}</thead>;
+};
+const TBody = (props: any) => {
+  return <tbody className="article-ele-tbody">{props.children}</tbody>;
+};
+const Tr = (props: any) => {
+  return <tr className="article-ele-tr">{props.children}</tr>;
+};
+const Th = (props: any) => {
+  return <th className="article-ele-th">{props.children}</th>;
+};
+const Td = (props: any) => {
+  return <td className="article-ele-td">{props.children}</td>;
+};
+
 function getMd(path: string) {
   const data = fetch(`http://localhost:3000/api/article/${path}`).then(
     (res) => {
@@ -86,6 +114,12 @@ const components = {
   ul: Ul,
   ol: Ol,
   li: Li,
+  table: Table,
+  thead: THead,
+  tbody: TBody,
+  tr: Tr,
+  th: Th,
+  td: Td,
 };
 
 const Article = (props: { params: { slug: ArticlePath } }) => {
